3fd8e58d695c906d2f6d7d545bd65917174a35bf
Jun <junzhengrice@gmail.com>
2016-03-07 14:39:48 -0600

    HW01 fixed a bug

--- Percolation.java
+++ Percolation.java
@@ -2,145 +2,168 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-	private int mat[][]; // recording index
-	private int N;
-	private WeightedQuickUnionUF node;
+    private int mat[][]; // recording index
+    private int N;
+    private WeightedQuickUnionUF node;
 
-	private void check(int i, int j) {
-		if (i > N || j > N || i <= 0 || j <= 0) {
-			throw new IndexOutOfBoundsException();
-		}
-	}
+    private void check(int i, int j) {
+        if (i > N || j > N || i <= 0 || j <= 0) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
 
-	/**
-	 * map (i,j) on linear coordinate
-	 * 
-	 * @i: [1,N]
-	 * @return: [0,N*N-1]
-	 */
-	private int linearize(int i, int j) {
-		return (i - 1) * N + j - 1;
-	}
+    /**
+     * map (i,j) on linear coordinate
+     * 
+     * @i: [1,N]
+     * @return: [0,N*N-1]
+     */
+    private int linearize(int i, int j) {
+        return (i - 1) * N + j - 1;
+    }
 
-	/**
-	 * i,j : [1,N]
-	 */
-	public void open(int i, int j) {
-		if (isOpen(i, j))
-			return;
-		int center = linearize(i, j);
-		int id = center;
+    /**
+     * i,j : [1,N]
+     */
+    public void open(int i, int j) {
+        if (isOpen(i, j))
+            return;
+        int center = linearize(i, j);
+        int id = center;
 
-		if (i > 1 && isOpen(i - 1, j)) {
-			int nid = mat[i - 2][j - 1];
-			if (id != nid) {
-				node.union(center, linearize(i - 1, j));
-				id = node.find(center);
-			}
-		}
-		if (i < N && isOpen(i + 1, j)) {
-			int nid = mat[i][j - 1];
-			if (id != nid) {
-				node.union(center, linearize(i + 1, j));
-				id = node.find(center);
-			}
-		}
-		if (j > 1 && isOpen(i, j - 1)) {
-			int nid = mat[i - 1][j - 2];
-			if (id != nid) {
-				node.union(center, linearize(i, j - 1));
-				id = node.find(center);
-			}
-		}
-		if (j < N && isOpen(i, j + 1)) {
-			int nid = mat[i - 1][j];
-			if (id != nid) {
-				node.union(center, linearize(i, j + 1));
-				id = node.find(center);
-			}
-		}
+        if (i > 1 && isOpen(i - 1, j)) {
+            int nid = mat[i - 2][j - 1];
+            if (id != nid) {
+                node.union(center, linearize(i - 1, j));
+                id = node.find(center);
+            }
+        }
+        if (i < N && isOpen(i + 1, j)) {
+            int nid = mat[i][j - 1];
+            if (id != nid) {
+                node.union(center, linearize(i + 1, j));
+                id = node.find(center);
+            }
+        }
+        if (j > 1 && isOpen(i, j - 1)) {
+            int nid = mat[i - 1][j - 2];
+            if (id != nid) {
+                node.union(center, linearize(i, j - 1));
+                id = node.find(center);
+            }
+        }
+        if (j < N && isOpen(i, j + 1)) {
+            int nid = mat[i - 1][j];
+            if (id != nid) {
+                node.union(center, linearize(i, j + 1));
+                id = node.find(center);
+            }
+        }
 
-		mat[i - 1][j - 1] = id;
-	}
+        mat[i - 1][j - 1] = id;
 
-	public boolean isOpen(int i, int j) {
-		check(i, j);
-		return mat[i - 1][j - 1] != -1;
-	}
+        if (i > 1 && isOpen(i - 1, j)) {
+            int nid = mat[i - 2][j - 1];
+            if (id != nid)
+                mat[i - 2][j - 1] = id;
+        }
+        if (i < N && isOpen(i + 1, j)) {
+            int nid = mat[i][j - 1];
+            if (id != nid)
+                mat[i][j - 1] = id;
+        }
+        if (j > 1 && isOpen(i, j - 1)) {
+            int nid = mat[i - 1][j - 2];
+            if (id != nid)
+                mat[i - 1][j - 2] = id;
+        }
+        if (j < N && isOpen(i, j + 1)) {
+            int nid = mat[i - 1][j];
+            if (id != nid)
+                mat[i - 1][j] = id;
+        }
+    }
 
-	public boolean isFull(int i, int j) {
-		if (!isOpen(i, j))
-			return false;
-		if (i == 1)
-			return true;
+    public boolean isOpen(int i, int j) {
+        check(i, j);
+        return mat[i - 1][j - 1] != -1;
+    }
 
-		int id = mat[i - 1][j - 1];
-		for (int k = 1; k <= N; k++) {
-			if (mat[0][k - 1] == id) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public boolean isFull(int i, int j) {
+        if (!isOpen(i, j))
+            return false;
+        if (i == 1)
+            return true;
 
-	public boolean percolates() {
-		for (int i = 1; i <= N; i++) {
-			if (isFull(N, i)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        int id = mat[i - 1][j - 1];
+        for (int k = 1; k <= N; k++) {
+            if (mat[0][k - 1] == id) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private void print() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.format("%2d ", mat[i][j]);
-			}
-			System.out.println();
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.format("%2d ", node.find(linearize(i + 1, j + 1)));
-			}
-			System.out.println();
-		}
-		// System.out.println();
-	}
+    public boolean percolates() {
+        for (int i = 1; i <= N; i++) {
+            if (isFull(N, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public Percolation(int N) {
-		if (N <= 0) {
-			throw new IllegalArgumentException();
-		}
-		this.N = N;
-		mat = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				mat[i][j] = -1;
-			}
-		}
-		node = new WeightedQuickUnionUF(N * N);
-	}
+    private void print() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.format("%2d ", mat[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.format("%2d ", node.find(linearize(i + 1, j + 1)));
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
-	private static void utest(Percolation pc, int N, int x, int y) {
-		pc.open(x, y);
-		pc.print();
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				int v = pc.isFull(i, j) ? 1 : 0;
-				System.out.format("%2d ", v);
-			}
-			System.out.println();
-		}
-		System.out.println(pc.percolates());
-	}
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        mat = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                mat[i][j] = -1;
+            }
+        }
+        node = new WeightedQuickUnionUF(N * N);
+    }
 
-	public static void main(String[] args) {
-		int N = 3;
-		Percolation pc = new Percolation(N);
-		utest(pc, N, 1, 1);
-		utest(pc, N, 1, 2);
-		utest(pc, N, 2, 2);
-		utest(pc, N, 3, 2);
-	}
+    private static void utest(Percolation pc, int N, int x, int y) {
+        pc.open(x, y);
+        pc.print();
+        // for (int i = 1; i <= N; i++) {
+        // for (int j = 1; j <= N; j++) {
+        // int v = pc.isFull(i, j) ? 1 : 0;
+        // System.out.format("%2d ", v);
+        // }
+        // System.out.println();
+        // }
+        System.out.println(pc.percolates());
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int N = 3;
+        Percolation pc = new Percolation(N);
+        utest(pc, N, 1, 1);
+        utest(pc, N, 2, 2);
+        utest(pc, N, 1, 2);
+        utest(pc, N, 3, 2);
+    }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,78 +1,79 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
+
 /*
-http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
-*/
+ http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+ */
 public class PercolationStats {
 
-	private int N; // matrix dimesion
-	private int T; // simulation times
-	private double[] fractions; //
+    private int N; // matrix dimesion
+    private int T; // simulation times
+    private double[] fractions; //
 
-	public double mean() {
-		return StdStats.mean(fractions);
-	}
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
 
-	private double var() {
-		return StdStats.var(fractions);
-	}
+    private double var() {
+        return StdStats.var(fractions);
+    }
 
-	public double stddev() {
-		return StdStats.stddev(fractions);
-	}
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
 
-	public double confidenceLo() {
-		return mean() - 1.96 * var() / Math.sqrt(T);
-	}
+    public double confidenceLo() {
+        return mean() - 1.96 * var() / Math.sqrt(T);
+    }
 
-	public double confidenceHi() {
-		return mean() + 1.96 * var() / Math.sqrt(T);
-	}
+    public double confidenceHi() {
+        return mean() + 1.96 * var() / Math.sqrt(T);
+    }
 
-	private void print() {
-		StdOut.println("mean = " + mean());
-		StdOut.println("stddev =" + stddev());
-		StdOut.println("95%s confidence interval = " + confidenceLo() + ", "
-				+ confidenceHi());
-		StdOut.println();
-	}
+    private void print() {
+        StdOut.println("mean = " + mean());
+        StdOut.println("stddev =" + stddev());
+        StdOut.println("95%s confidence interval = " + confidenceLo() + ", "
+                + confidenceHi());
+        StdOut.println();
+    }
 
-	private void simulate() {
-		int bound = N * N;
-		for (int i = 0; i < T; i++) {
-			Percolation node = new Percolation(N);
-			int opened = 0;
-			while (!node.percolates() && opened < bound) {
-				int rand = StdRandom.uniform(0, bound);
-				int row = (int) (rand / N) + 1;
-				int col = (int) (rand % N) + 1;
-				if (!node.isOpen(row, col)) {
-					node.open(row, col);
-					opened++;					
-					//node.print();
-				}
-			}
-			fractions[i] = (double) opened / (double) bound;
-		}
-	}
+    private void simulate() {
+        int bound = N * N;
+        for (int i = 0; i < T; i++) {
+            Percolation node = new Percolation(N);
+            int opened = 0;
+            while (!node.percolates() && opened < bound) {
+                int rand = StdRandom.uniform(0, bound);
+                int row = (int) (rand / N) + 1;
+                int col = (int) (rand % N) + 1;
+                if (!node.isOpen(row, col)) {
+                    node.open(row, col);
+                    opened++;
+                    // node.print();
+                }
+            }
+            fractions[i] = (double) opened / (double) bound;
+        }
+    }
 
-	public PercolationStats(int N, int T) {
-		if (N < 1 || T < 1) {
-			throw new IllegalArgumentException();
-		}
-		this.N = N;
-		this.T = T;
-		fractions = new double[T];
-		simulate();
-		print();
-	}
+    public PercolationStats(int N, int T) {
+        if (N < 1 || T < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        this.T = T;
+        fractions = new double[T];
+        simulate();
+        print();
+    }
 
-	public static void main(String[] args) {
-		if (args.length >= 2) {
-			int N = Integer.parseInt(args[0]);
-			int T = Integer.parseInt(args[1]);
-			new PercolationStats(N, T);
-		}
-	}
+    public static void main(String[] args) {
+        if (args.length >= 2) {
+            int N = Integer.parseInt(args[0]);
+            int T = Integer.parseInt(args[1]);
+            new PercolationStats(N, T);
+        }
+    }
 }
